bc8a2d9188c2cd051f06325c5b5d58817c8fd3c6
KaikAciole <kaikaciole1.0@gmail.com>
2024-04-20 00:48:32 -0300

    atualizando nova secao de conteudo

---
+++ Portfolio/script.js
@@ -0,0 +1,14 @@
+function eventoClick(){
+    var teste = document.getElementById("tan");
+    var ton = document.getElementById("none"); 
+    teste.style.display = "none"
+    ton.style.display = "block"
+}
+
+function eventoTeste(){
+    var toto = document.getElementById("tan");
+    var tente = document.getElementById("none"); 
+    tente.style.display = "none";
+    toto.style.display = "flex";
+    
+}
