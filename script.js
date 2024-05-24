1d6337ba0dc6641037e4b8584248a916c572c665
KaikAciole <kaikaciole1.0@gmail.com>
2024-05-24 18:14:06 -0300

    update

--- script.js
+++ script.js
@@ -37,24 +37,48 @@ function eventCard3Out(){
     let x = document.getElementById("img-card-3");
     let y = document.getElementById("text-card-3");
     x.style.display = "flex";
-    y.style.display = "none"
+    y.style.display = "none";
 }
 
 function eventCard4Over(){
     let x = document.getElementById("img-card-4");
     let y = document.getElementById("text-card-4");
     x.style.display = "none";
-    y.style.display = "block"
+    y.style.display = "block";
 }
 
 function eventCard4Out(){
     let x = document.getElementById("img-card-4");
     let y = document.getElementById("text-card-4");
     x.style.display = "flex";
-    y.style.display = "none"
+    y.style.display = "none";
 }
 
 function eventLink(){
     let x = document.getElementById("curriculo");
     x.style.color = "gray";
+}
+
+function eventVerMais(){
+    let x = document.getElementsByClassName("unview");
+    let btn1 = document.getElementById("btn1");
+    let btn2 = document.getElementById("btn2");
+    btn1.style.display = "none";
+    btn2.style.display = "flex"
+    for(let item of x){
+        item.style.display = "flex"
+    }
+}
+
+function eventVerMenos(){
+    let x = document.getElementsByClassName("unview");
+    let btn1 = document.getElementById("btn1");
+    let btn2 = document.getElementById("btn2");
+    btn1.style.display = "flex";
+    btn2.style.display = "none"
+    for(let item of x){
+        setTimeout(function(){
+            item.style.display = "none"
+        }, 200)
+    }
 }
